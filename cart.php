c81f3794cde2d1729574086c24f4b595189940ed
Ancar <ancar>
2017-12-15 12:42:03 +0200

    index + cart
    index + cart = complete

--- cart.php
+++ cart.php
@@ -1,38 +1,62 @@
 <?php
-
 require_once("common.php");
-
 $result = $conn->query($sql);
 
-if(!empty($_GET["action"])) {  
-    if($_GET["action"] == "remove") {
-        foreach ($_SESSION["cart"] as $keys => $values) {
-            if ($values["Id"] == $_GET["id"]) {
-                unset($_SESSION["cart"][$keys]);
-                $_SESSION["cart"] = array_merge($_SESSION["cart"]);
+if(!empty($_GET["action"]) && $_GET["action"] == "remove") {  
+    foreach ($_SESSION["cart"] as $keys => $values) {
+        if ($values["Id"] == $_GET["id"]) {
+            unset($_SESSION["cart"][$keys]);
+            $_SESSION["cart"] = array_merge($_SESSION["cart"]);
+        }
+    }
+}
+
+$cartProducts = array();
+if(!empty($_SESSION["cart"]) && $result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        foreach (array_column($_SESSION["cart"], "Id") as $keys => $values) {
+            if($values == $row["Id"]) {
+                $cartProducts[] = array(
+                    "id" => $row["Id"],
+                    "title" => $row["Title"],
+                    "description" => $row["Description"],
+                    "price" => $row["Price"],
+                    "image" => $row["Image"]
+                );
             }
         }
     }
 }
 
-
-
-if(isset($_POST["checkout"])) {
-	$to = "ancarusu2000@gmail.com";
-	$from = $_POST["contactDetails"];
-	$name = $_POST["name"];
-	$subject = "Form submision";
-	$headers = "From: " . $from;
-        foreach ($_SESSION["cart"] as $key => $value) {
-        $x[] = $value["Id"];
-    }
-    $myvar = implode(", ", $x);
-    print_r($myvar);
-    $message = $name . " wrote: " . $_POST["comments"] . "Order: " .$myvar;
-	mail($to, $subject, $message, $headers, $myvar);
-
+$order = "";
+$totalsum = 0;
+$i = 0;
+while ($i < count($cartProducts)) {
+    $order .= $cartProducts[$i]["title"] . " " . $cartProducts[$i]["price"] . "\r\n";
+    $totalsum += $cartProducts[$i]["price"];
+    $i++;
 }
 
+$nameErr = $contactDetailsErr = "";
+if(isset($_POST["checkout"])) {
+	$to = "ancarusu2000@gmail.com";
+	$contactDetails = test_user_input($_POST["contactDetails"]);
+    $name = test_user_input($_POST["name"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+        $nameErr = "Only letters and white space allowed"; 
+    } 
+    if (!filter_var($contactDetails, FILTER_VALIDATE_EMAIL)) {
+        $contactDetailsErr = "Email is not valid";
+    }
+    $comments = test_user_input( $_POST["comments"]);
+	$subject = "Form submision";
+	$headers = "From: " . $contactDetails;
+    $message = $name . " wrote: " . $comments . "\r\n". "Order: " . "\r\n" . $order ."\r\n" . 
+    "Total: " . $totalsum;
+    if(preg_match("/^[a-zA-Z ]*$/",$name) && (filter_var($contactDetails, FILTER_VALIDATE_EMAIL))) {
+	   mail($to, $subject, $message, $headers);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -42,33 +66,32 @@ if(isset($_POST["checkout"])) {
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<?php if(!empty($_SESSION["cart"])): ?>
-	<?php if($result->num_rows > 0): ?>
-    	<?php while($row = $result->fetch_assoc()): ?>
-        	<?php foreach (array_column($_SESSION["cart"], "Id") as $keys => $values):?> 
-        		<?php if($values == $row["Id"]): ?>
-                	<div class="product"> 
-                		<div class="image">
-                            <img src="<?= $row["Image"]?>">
-                        </div>
-                        <div class="productdetails">
-                            <div class="productTitle"><?= $row["Title"]?></div>
-                            <div class="productDescription"><?=$row["Description"]?></div>
-                            <div class="productPrice"><?=$row["Price"]?></div>
-                            <a href="cart.php?action=remove&amp;id=<?= $row["Id"] ?>" class="remove"><?=translate("Remove")?></a>
-                            </div>
-                        </div>
-                <?php endif; ?>
-        	<?php endforeach;?> 
-        <?php endwhile; ?>
-    <?php endif; ?>
-<?php endif; ?>
+<?php foreach (array_column($_SESSION["cart"], "Id") as $keys => $values): $i = 0; ?> 
+    <?php while ($i < count($_SESSION["cart"])): ?>
+        <?php if($values == $cartProducts[$i]["id"]): ?>
+            <div class="product"> 
+                <div class="image">
+                    <img src="<?= $cartProducts[$i]["image"] ?>">
+                </div>
+                <div class="productdetails">
+                    <div class="productTitle"><?= $cartProducts[$i]["title"] ?></div>
+                    <div class="productDescription"><?= $cartProducts[$i]["description"] ?></div>
+                    <div class="productPrice"><?= $cartProducts[$i]["price"] ?></div>
+                    <a href="cart.php?action=remove&amp;id=<?= $cartProducts[$i]["id"] ?>" class="remove"><?= translate("Remove") ?></a>
+                </div>
+            </div>
+        <?php endif; ?>
+    <?php $i++; endwhile; ?>
+<?php endforeach;?> 
+
 <div class="linkToIndex">
-    <a href="index.php"><?=translate("Go to index")?></a>
+    <a href="index.php"><?= translate("Go to index") ?></a>
 </div>
-<form action="cart.php" method="post">
-	<input type="text" name="name" placeholder="Name"><br />
-	<input type="text" name="contactDetails" placeholder="Contact details"><br />
+<form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+	<input type="text" name="name" placeholder="Name" required="required">
+    <span><?= $nameErr ?></span><br />
+	<input type="text" name="contactDetails" placeholder="Contact details" required="required">
+    <span><?= $contactDetailsErr ?></span><br />
 	<input type="text" name="comments" placeholder="Comments"><br />
 	<input type="submit" name="checkout" value="Checkout">
 </form>
--- common.php
+++ common.php
@@ -13,13 +13,18 @@ if ($conn->connect_error) {
     die("Conection error: " . $conn->connect_error);
 }
 
+$sql = "SELECT * FROM productsnew";
+
 function translate($string) {
 	return $string;
 }
 
-$sql = "SELECT * FROM productsnew";
-
-
+function test_user_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 
 
 
