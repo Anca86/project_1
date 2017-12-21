4f1988102c6f866a0c99bbcb35e9abbf04c9c3fe
Ancar <ancar>
2017-12-21 16:05:48 +0200

    product
    product update button fixed - can update after updated once

--- product.php
+++ product.php
@@ -42,19 +42,19 @@ if(isset($_POST["Update"])) {
 	$description = test_user_input($_POST["Description"]);
 	$price = test_user_input($_POST["Price"]);
 	if(!empty($title) && !empty($description) && !empty($price)) {
-		$sql = $conn->prepare("UPDATE productsnew set Title =?, Description =?, Price =? WHERE Id=?");
-		$sql->bind_param("ssii", $title, $description, $price, $editId);
+		$stmt = $conn->prepare("UPDATE productsnew set Title =?, Description =?, Price =? WHERE Id=?");
+		$stmt->bind_param("ssii", $title, $description, $price, $editId);
 		if(is_uploaded_file($_FILES["file"]["tmp_name"])) {
-			$sql = $conn->prepare("UPDATE productsnew set Title =?, Description =?, Price =?, Image =? WHERE Id=?");
-			$sql->bind_param("ssisi", $title, $description, $price, $target_file, $editId);
-			if($sql->execute()) {
-				$fileId = mysqli_stmt_insert_id($sql);
+			$stmt = $conn->prepare("UPDATE productsnew set Title =?, Description =?, Price =?, Image =? WHERE Id=?");
+			$stmt->bind_param("ssisi", $title, $description, $price, $target_file, $editId);
+			if($stmt->execute()) {
+				$fileId = mysqli_stmt_insert_id($stmt);
 				copy($_FILES["file"]["tmp_name"], __DIR__ . "/uploads/".$target_file);
 			}
 		}
-		$sql->execute();
+		$stmt->execute();
 	}
-	$sql->close();
+	$stmt->close();
 	$succes = _UPDATE_PRODUCT;
 }
 ?>
@@ -73,7 +73,8 @@ if(isset($_POST["Update"])) {
 	value="<?= (isset($_POST["edit"])) ? translate($row["Description"]) : $description; ?>"><br />
 	<input type="text" name="Price" required="required" placeholder="Price"
 	value="<?= (isset($_POST["edit"])) ? translate($row["Price"]) : $price; ?>" ><br />
-	<input type="hidden" name="hidden_id" value="<?php if(isset($_POST["edit"])) :?><?= translate($row["Id"])?><?php endif; ?>">
+	<input type="hidden" name="hidden_id" 
+	value="<?php if(isset($_POST["edit"]) || isset($_POST["Update"])) :?><?= translate($row["Id"])?><?php endif; ?>">
 	<input type="file" name="file" id="file"><br /><br />
 	<a href="products.php">Products</a>
 	<input type="submit" name="<?= $buttonValue ?>" value="<?= $buttonValue?>">
