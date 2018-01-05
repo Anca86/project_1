67789a87c253f2e7e1d602b2286c61c16e66384d
Ancar <ancar>
2018-01-05 13:43:14 +0200

    1
    1

--- config.dist.php
+++ config.dist.php
@@ -4,7 +4,6 @@ define("_DB_HOST", "localhost");
 define("_DB_USER", "");
 define("_DB_PASSWORD", "");
 define("_DB_DATABASE", "");
-
 $adminUsername = "";
 $adminPassword = "";
 $to = "ancarusu2000@gmail.com";
