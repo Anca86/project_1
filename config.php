ef965ad37046086face76bc58c317f7e166f5944
Anca86 <ancarusu2000@gmail.com>
2018-01-19 12:28:34 +0200

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-define("_DB_HOST", "localhost");
-define("_DB_USER", "root");
-define("_DB_PASSWORD", "ancar");
-define("_DB_DATABASE", "project1");
-define("_EMAIL", "ancarusu2000@gmail.com");
-define("_ADMIN_USERNAME", "anca");
-define("_ADMIN_PASSWORD", "123");
