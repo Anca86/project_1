186c0213d2d188a5d26787b52994519db181d360
Ancar <ancar>
2018-01-29 11:02:31 +0200

    products fixed
    products fixed - can add product

--- product.php
+++ product.php
@@ -7,30 +7,34 @@ if(!$_SESSION["admin"]){
 }
 
 $uploadOk = 1;
-$title = $description = $price = $uploadMsg = "";
+$title = $description = $price = $uploadMsg = $errMsg = "";
+$isId = isset($_GET["id"]);
+$isSave = isset($_POST["save"]);
 
-if(isset($_POST["save"])) {
+if($isSave) {
     $target_file = time() . basename($_FILES["file"]["name"]);
     $title = clean_user_input($_POST["title"]);
     $description = clean_user_input($_POST["description"]);
     $price = clean_user_input($_POST["price"]);
     $stmt = "";
-    // if action is equal to add
-    if(!isset($_GET["id"])) {
+    //if add
+    if(!$isId) {
         if(!empty($title) && !empty($description) && !empty($price) && getimagesize($_FILES["file"]["tmp_name"])) {
             $stmt = $conn->prepare("INSERT INTO products (title, description, price, image) VALUES (?, ?, ?, ?)");
             $stmt->bind_param("ssis", $title, $description, $price, $target_file);
             $uploadMsg = translate("Product was added!");
-        }
-    // if action is equal to edit
-    } else {
+        } 
+    } else { // if edit
         $editId = $_GET["id"];
-        if(!empty($title) && !empty($description) && !empty($price )) {
-            if(is_uploaded_file($_FILES["file"]["tmp_name"])) {
+        if(!empty($title) && !empty($description) && !empty($price)) {
+            // if we want to update the image
+            if(is_uploaded_file($_FILES["file"]["tmp_name"])) { 
                 if(getimagesize($_FILES["file"]["tmp_name"])) {
                     $stmt = $conn->prepare("UPDATE products set title =?, description =?, price =?, image =? WHERE id=?");
                     $stmt->bind_param("ssisi", $title, $description, $price, $target_file, $editId);
                     $uploadMsg = translate("Product was updated!");
+                } else {
+                    $uploadMsg = translate("File uploaded is not an image!");
                 }
             } else {
                 $stmt = $conn->prepare("UPDATE products set title =?, description =?, price =? WHERE id=?");
@@ -39,17 +43,17 @@ if(isset($_POST["save"])) {
             }
         }
     }
-
     if($stmt && $stmt->execute()) {
         move_uploaded_file($_FILES["file"]["tmp_name"], __DIR__ . "/uploads/".$target_file);
         $stmt->close();
-    } else {
-        $uploadMsg = "Something went wrong! Please try again!";
+    } 
+    else {
+        $errMsg = translate("Something went wrong! Please try again!");
     }
 }
 
 //display product for edit
-if(isset($_GET["action"]) && $_GET["action"] == "edit" && !isset($_POST["save"])) {
+if(isset($_GET["action"]) && $_GET["action"] == "edit" && !$isSave) {
     $editId = $_GET["id"];
     $stmt = $conn->prepare("SELECT * FROM products WHERE id=?");
     $stmt->bind_param("i", $editId);
@@ -72,6 +76,7 @@ $conn->close();
 <body>
 <form method="post" enctype="multipart/form-data">
     <span><?= $uploadMsg ?></span><br />
+    <span><?= $errMsg ?></span><br />
     <input type="text" name="title" required="required" placeholder="<?= translate("Title") ?>"
     value="<?= $title; ?>" >
     <br />
@@ -82,7 +87,7 @@ $conn->close();
     <input type="file" name="file" id="file"><br /><br />
     <a href="products.php"><?= translate("Products") ?></a>
     <input type="submit" name="save" 
-    value="<?= (isset($_GET["id"])) ? "Update": "Save"; ?>">
+    value="<?= $isId ? "Update": "Save"; ?>">
 </form>
 </body>
 </html>
